79cbaa3ce3b86143319c1a6e2c504348cc3750ba
Julien Lin <julien.lin@ensta-paris.fr>
2020-01-23 01:49:59 +0100

    create screen class

---
+++ src/screen.cpp
@@ -0,0 +1,24 @@
+#include "screen.hpp"
+
+Screen::Screen(int H, int W, double pix_size, vector_t v_H, vector_t v_W,
+               position_t screen_pos)
+    : m_H(H), m_W(W), m_pix_size(pix_size), m_v_H(v_H), m_v_W(v_H),
+      m_screen_pos(screen_pos), m_pixels(m_H * m_W), m_pix_pos(m_H * m_W) {
+  int n = m_H * m_W;
+  m_pix_pos[0] = position_t(m_screen_pos);
+  double half_px_size = m_pix_size / 2;
+  vector_t cur_v_H = m_v_H;
+  vector_t cur_v_W = m_v_W;
+  for (int i = 0; i < m_H; i++) {
+    cur_v_W = m_v_W;
+    for (int j = 0; j < m_W; j++) {
+      // calcul des futurs position centrale ( il faut sauvegarder ou utilisé le
+      // tableau existant )
+      m_pix_pos[i * m_W + j] =
+          m_screen_pos + half_px_size * cur_v_H + half_px_size * cur_v_W;
+      cur_v_W += m_v_W;
+    }
+      cur_v_H += m_v_H;
+  }
+
+}
---
+++ src/screen.hpp
@@ -0,0 +1,37 @@
+#ifndef __SCREEN_H_
+#define __SCREEN_H_
+
+#include "types.hpp"
+#include <utility>
+#include <vector>
+
+class Screen {
+private:
+  int m_H;
+  int m_W;
+  double
+      m_pix_size; /**< Size of a pixel in the model. The pixel is a square. */
+
+  vector_t m_v_H; /**<  Vector that define the direction and orientation of the
+                     screen in Height. Since M_POS is the position of top left
+                     most pixel. */
+  vector_t m_v_W; /**<  Same as m_v_H but in the width direction.*/
+
+  position_t m_screen_pos; /**< position of the of the most top left pixel. It
+                              define the center of the pixel.*/
+  std::vector<RGBColor> m_pixels;
+
+  std::vector<position_t>
+      m_pix_pos; /**<  Vector of position that refers to the center of pixel
+                (i,j) (W,H) by [ j * m_W + i ] */
+
+public:
+  Screen(int H, int W, double pix_size, vector_t v_H, vector_t v_W,
+         position_t screen_pos);
+
+  ~Screen() {}
+  int H() { return m_H; }
+  int W() { return m_W; }
+};
+
+#endif // __SCREEN_H_
