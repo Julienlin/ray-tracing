53baeaa41576e030d6d34a7629ea92feeec0afa5
Julien Lin <julien.lin@ensta-paris.fr>
2020-01-23 01:50:39 +0100

    create scene sphere class

---
+++ src/scene_sphere.cpp
@@ -0,0 +1,16 @@
+#include "scene_sphere.hpp"
+
+double SceneSphere::intersecDist(const Ray &ray) {
+  position_t v = ray.getPos() - m_center;
+  double s_v_d = (v * ray.getDirection());
+  s_v_d *= (v * ray.getDirection());
+  double s_v = v * v, discriminant = s_v_d - s_v + m_radius * m_radius;
+  if(discriminant < 0 )
+     return 0;
+  double res = -s_v_d + std::max(- std::sqrt(discriminant), std::sqrt(discriminant)) ;
+  return res;
+}
+
+vector_t SceneSphere::getNormal(position_t &pos) {
+  return vector_t(pos - m_center).normalize();
+}
---
+++ src/scene_sphere.hpp
@@ -0,0 +1,44 @@
+#ifndef __SCENE_SPHERE_H_
+#define __SCENE_SPHERE_H_
+
+#include "light_source.hpp"
+#include "scene_base_object.hpp"
+#include "types.hpp"
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+class SceneSphere : public SceneBaseObject {
+private:
+  position_t m_center;
+  double m_radius;
+
+public:
+  SceneSphere() : m_center(POSITION_T_NULL), m_radius(0) {}
+  SceneSphere(ObjectBaseSurface *surface, position_t center, double radius)
+      : SceneBaseObject(surface), m_center(center), m_radius(radius) {}
+  ~SceneSphere() {}
+  // TODO: implements the followings function for testing
+  genRays_t generateRays(const Ray &incident_ray);
+  /**
+   * @brief Test whether the ray is intersecting the object.
+   *
+   * @param ray
+   *
+   * @return true if the ray is intersecting the object if not false.
+   */
+  double intersecDist(const Ray &ray);
+
+  Ray generateReflectionRay(const Ray &incident);
+  Ray generateRefractionRay(const Ray &incident);
+  /**
+   * @brief get the normal to the surface passing by POS.
+   *
+   * @param pos
+   *
+   * @return the normal to the surface passing by POS.
+   */
+  vector_t getNormal(position_t &pos);
+};
+
+#endif // __SCENE_SPHERE_H_
