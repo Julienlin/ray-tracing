21f11ef80c4d01923144e9d9b19f555fb45366ab
Julien Lin <julien.lin@ensta-paris.fr>
2020-01-23 01:51:45 +0100

    create main class

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "light_source.hpp"
+#include "objet_base_surface.hpp"
+#include "ray.hpp"
+#include "ray_engine.hpp"
+#include "scene_base_object.hpp"
+#include "scene_cube.hpp"
+#include "scene_sphere.hpp"
+#include "screen.hpp"
+#include "types.hpp"
+#include <iostream>
+#include <vector>
+int main(int argc, char *argv[]) {
+
+  std::vector<SceneBaseObject *> objects;
+  SceneSphere sphere;
+  return 0;
+}
