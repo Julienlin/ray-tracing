6eca5643bdcd6c656bf9d6ce244a9df79ca41546
Julien Lin <julien.lin@ensta-paris.fr>
2020-01-23 01:48:49 +0100

    add comment

--- src/scene_base_object.hpp
+++ src/scene_base_object.hpp
@@ -15,12 +15,28 @@ public:
   SceneBaseObject(const SceneBaseObject &obj) = default;
   SceneBaseObject(SceneBaseObject &&obj) = default;
   virtual ~SceneBaseObject(){};
+  virtual double intersecDist(const Ray &ray) = 0;
+  /**
+   * Generate a set of rays for implementation of several physic phenomenon.
+   * This function calls several other virtual functions for the generation of
+   * rays.
+   *
+   * @param incident_ray the incident ray.
+   *
+   * @return set of generated rays
+   */
   virtual genRays_t generateRays(const Ray &incident_ray) = 0;
-  virtual bool isIntersecting(const Ray &ray) = 0;
   virtual Ray generateReflectionRay(const Ray &incident) = 0;
   virtual Ray generateRefractionRay(const Ray &incident) = 0;
+
+  /**
+   * @brief get the normal to the surface passing by POS.
+   *
+   * @param pos
+   *
+   * @return the normal to the surface passing by POS.
+   */
   virtual vector_t getNormal(position_t &pos) = 0;
-  virtual double getLightSourceDirection(position_t &pos) = 0;
 };
 
 #endif // __SCENE_BASE_OBJECT_H_
