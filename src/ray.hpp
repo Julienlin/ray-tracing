f9b2c8b4b482318aab45d3140b59bd206bf02715
Julien Lin <julien.lin@ensta-paris.fr>
2020-01-30 01:18:50 +0100

    add fondamental ray attribute

--- src/ray.hpp
+++ src/ray.hpp
@@ -11,17 +11,19 @@ private:
   vector_t m_direction;
   double m_intensity;
   RGBColor m_color;
+  Ray *m_fondamental;
 
 public:
   Ray()
       : m_pos(POSITION_T_NULL), m_direction(POSITION_T_NULL), m_intensity(0),
         m_color() {}
-  Ray(position_t pos, vector_t direction, double intensity, RGBColor color)
+  Ray(position_t pos, vector_t direction, double intensity, RGBColor color,
+      Ray *fondamental = nullptr)
       : m_pos(pos), m_direction(direction), m_intensity(intensity),
-        m_color(color) {}
- Ray(position_t pos, vector_t direction)
-      : m_pos(pos), m_direction(direction), m_intensity(0),
-        m_color() {}
+        m_color(color), m_fondamental(fondamental) {}
+  Ray(position_t pos, vector_t direction)
+      : m_pos(pos), m_direction(direction), m_intensity(0), m_color(),
+        m_fondamental(nullptr) {}
 
   position_t getPos() const { return m_pos; };
   vector_t getDirection() const { return m_direction; }
@@ -31,7 +33,7 @@ public:
   void setDirection(const vector_t &direction);
   void setIntensity(double intensity) { m_intensity = intensity; }
   void setColor(RGBColor color) { swap(color, m_color); }
-  Ray& operator=(const Ray& );
+  Ray &operator=(const Ray &);
 };
 
 using genRays_t = std::tuple<Ray, Ray, Ray>;
