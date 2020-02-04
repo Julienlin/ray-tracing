710ed5b0480aa4178588a86de35261623a8219d1
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-04 11:50:16 +0100

    create main header

---
+++ src/main.hpp
@@ -0,0 +1,18 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+#include "light_source.hpp"
+#include "objet_base_surface.hpp"
+#include "ray.hpp"
+#include "ray_engine.hpp"
+#include "scene_base_object.hpp"
+#include "scene_cube.hpp"
+#include "scene_sphere.hpp"
+#include "screen.hpp"
+#include "light_source.hpp"
+#include "ray_engine.hpp"
+#include "types.hpp"
+#include <iostream>
+#include <vector>
+
+#endif // __MAIN_H_
