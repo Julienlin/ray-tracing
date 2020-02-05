3af4ab05399ccd9b50c4826deb8ef831558a214c
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-05 18:01:39 +0100

    Removing intensity attribute;

--- src/ray.hpp
+++ src/ray.hpp
@@ -10,27 +10,24 @@ class Ray
 private:
   position_t m_pos;
   vector_t m_direction;
-  double m_intensity;
   RGBColor m_color;
 
 public:
   Ray()
-      : m_pos(POSITION_T_NULL), m_direction(POSITION_T_NULL), m_intensity(0),
+      : m_pos(POSITION_T_NULL), m_direction(POSITION_T_NULL),
         m_color() {}
-  Ray(position_t pos, vector_t direction, double intensity, RGBColor color,
+  Ray(position_t pos, vector_t direction, RGBColor color,
       Ray *fondamental = nullptr)
-      : m_pos(pos), m_direction(direction), m_intensity(intensity),
+      : m_pos(pos), m_direction(direction),
         m_color(color) {}
   Ray(position_t pos, vector_t direction)
-      : m_pos(pos), m_direction(direction), m_intensity(0), m_color() {}
+      : m_pos(pos), m_direction(direction), m_color() {}
 
   position_t getPos() const { return m_pos; };
   vector_t getDirection() const { return m_direction; }
-  double getIntensity() const { return m_intensity; }
   RGBColor getColor() const { return m_color; };
   void setPos(const position_t &pos);
   void setDirection(const vector_t &direction);
-  void setIntensity(double intensity) { m_intensity = intensity; }
   void setColor(RGBColor color) { swap(color, m_color); }
   Ray &operator=(const Ray &);
 };
