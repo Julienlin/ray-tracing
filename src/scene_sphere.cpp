43a7611a4bd65153964004dc43defdf37b95744d
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-05 11:53:56 +0100

    fixing intesction to a treshold

--- src/scene_sphere.cpp
+++ src/scene_sphere.cpp
@@ -7,9 +7,19 @@ double SceneSphere::intersecDist(const Ray &ray)
   double s_v_d = (v * normal_direc);
   // s_v_d *= s_v_d;
   double s_v = v * v, discriminant = s_v_d * s_v_d - s_v + m_radius * m_radius;
+  // std::cout << "ray.getPos() : " << ray.getPos() << "\tm_center : " << m_center << "\tv : " << v << "\tnormal_direc : " << normal_direc << "\ts_v_d : " << s_v_d << "\ts_v : " << s_v << "\tdiscriminant : " << discriminant << std::endl;
+  double res = -1;
   if (discriminant < 0)
-    return -1;
-  double res = -s_v_d - std::sqrt(discriminant) > 0 ? -s_v_d - std::sqrt(discriminant) : -s_v_d + std::sqrt(discriminant);
+    return res;
+  if (-s_v_d - std::sqrt(discriminant) > 1e-10)
+  {
+    res = -s_v_d - std::sqrt(discriminant);
+  }
+  else if (-s_v_d + std::sqrt(discriminant) > 1e-10)
+  {
+    res = -s_v_d + std::sqrt(discriminant);
+  }
+
   return res;
 }
 
