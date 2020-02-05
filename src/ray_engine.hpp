fb85d9d3215a4bc51881997cd0fe3ec7550b54de
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-05 11:52:54 +0100

    fixing issue with the engine

--- src/ray_engine.hpp
+++ src/ray_engine.hpp
@@ -6,10 +6,12 @@
 #include "scene_base_object.hpp"
 #include "screen.hpp"
 #include "types.hpp"
+#include <iostream>
 #include <limits>
 #include <vector>
 
-class RayEngine {
+class RayEngine
+{
 protected:
   std::vector<Ray> m_rays;
   std::vector<SceneBaseObject *> m_objects;
@@ -27,6 +29,7 @@ public:
             position_t &observer_pos, int deepth = 0)
       : m_rays(rays), m_objects(objects), m_sources(sources), m_screen(screen),
         m_obs_pos(observer_pos), m_deepth(deepth) {}
+
   RayEngine(std::vector<SceneBaseObject *> &objects,
             std::vector<LightSource> &sources, Screen &screen,
             position_t &obs_pos, int deepth = 0)
@@ -35,16 +38,18 @@ public:
   RayEngine(const RayEngine &engine) = default;
   RayEngine(RayEngine &&engine) = default;
   virtual ~RayEngine() {}
-  virtual void compute()=0;
+  virtual void compute() = 0;
   Screen get_screen() { return m_screen; }
 };
 
-class RayCastingEngine : public RayEngine {
+class RayCastingEngine : public RayEngine
+{
 public:
   RayCastingEngine(std::vector<SceneBaseObject *> &objects,
                    std::vector<LightSource> &sources, Screen &screen,
                    position_t &observer_pos)
-      : RayEngine(objects, sources, screen, observer_pos) {
+      : RayEngine(objects, sources, screen, observer_pos)
+  {
     m_rays.resize(m_screen.H() * m_screen.W());
   }
   RayCastingEngine(const RayCastingEngine &engine) = default;
@@ -52,7 +57,7 @@ public:
   void compute();
   obj_dist_t get_intersection(Ray &);
   void get_reachable_sources(position_t, std::vector<source_vect_t> &);
-  Ray make_reflect_ray(const position_t &,SceneBaseObject*, source_vect_t&);
+  Ray make_reflect_ray(const position_t &, SceneBaseObject *, source_vect_t &);
 };
 
 #endif // __RAY_CASTING_H_
