f5a497b5eb9f1da864bbbeb7ba8b3aa8196a7f5f
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-05 11:50:06 +0100

    overriding operators for position_t and and constant

--- src/types.hpp
+++ src/types.hpp
@@ -3,14 +3,15 @@
 
 #include <cmath>
 #include <cstdint>
-#include <tuple>
 #include <ostream>
+#include <tuple>
 
-class position_t {
+class position_t
+{
 public:
   position_t() : x(0), y(0), z(0) {}
   double x, y, z;
-  position_t(double x, double y, double z) : x(x), y(y), z(y) {}
+  position_t(double x, double y, double z) : x(x), y(y), z(z) {}
   position_t(const position_t &pos) = default;
   position_t(position_t &&pos) = default;
 
@@ -19,7 +20,7 @@ public:
   position_t operator*(const double c);
   position_t operator+(const position_t &pos);
   position_t operator-(const position_t &pos);
-  position_t& operator=(const position_t &pos);
+  position_t &operator=(const position_t &pos);
   position_t &operator+=(const position_t &pos);
   position_t &operator-=(const position_t &pos);
   position_t &operator*=(const double coef);
@@ -34,16 +35,25 @@ public:
   // void setX(double i){ x = i; }
   // void setY(double i){ y = i; }
   // void setZ(double i){ z = i; }
+
+  position_t operator^(const position_t &);
+  position_t operator-() const;
+  position_t operator-();
+  bool operator==(const position_t &);
+  friend std::ostream &operator<<(std::ostream &os, const position_t &pos);
 };
 
-position_t operator*(const double , const position_t );
-position_t operator*( double ,  position_t );
+position_t operator*(const double, const position_t);
+position_t operator*(double, position_t);
 
 using vector_t = position_t;
 
-class RGBColor {
+class RGBColor
+{
 private:
   unsigned char m_red, m_green, m_blue;
+  unsigned char cap(unsigned) const;
+  static const unsigned char threshold = 255;
 
 public:
   RGBColor() : m_red(0), m_green(0), m_blue(0) {}
@@ -52,19 +62,22 @@ public:
   RGBColor(const RGBColor &color) = default;
   RGBColor(RGBColor &&color) = default;
   void swap(RGBColor &other);
-  RGBColor& operator=(const RGBColor&);
-  RGBColor operator*(const double&) const;
-  friend std::ostream& operator<<( std::ostream& os, const RGBColor& color);
+  RGBColor &operator=(const RGBColor &);
+  RGBColor operator*(const double &)const;
+  friend std::ostream &operator<<(std::ostream &os, const RGBColor &color);
 };
 
-
 void swap(RGBColor &, RGBColor &);
-RGBColor operator*(double , const RGBColor& );
+RGBColor operator*(double, const RGBColor &);
 
 const position_t POSITION_T_NULL = position_t(0, 0, 0);
-const vector_t E1 = vector_t(1,0,0);
-const vector_t E2 = vector_t(0,1,0);
-const vector_t E3 = vector_t(0,0,1);
-const RGBColor RGB_WHITE = RGBColor(255,255,255);
-const RGBColor RGB_BLACK = RGBColor(0,0,0);
+const vector_t E1 = vector_t(1, 0, 0);
+const vector_t E2 = vector_t(0, 1, 0);
+const vector_t E3 = vector_t(0, 0, 1);
+const RGBColor RGB_WHITE = RGBColor(255, 255, 255);
+const RGBColor RGB_BLACK = RGBColor(0, 0, 0);
+const RGBColor RGB_RED = RGBColor(255, 0, 0);
+const RGBColor RGB_GREEN = RGBColor(0, 255, 0);
+const RGBColor RGB_BLUE = RGBColor(0, 0, 255);
+
 #endif // __TYPES_H_
