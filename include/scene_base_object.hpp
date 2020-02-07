b4da085dea7a4f8973bf84bee4353066651bf397
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-07 09:56:22 +0100

    Changing default parameter

--- include/scene_base_object.hpp
+++ include/scene_base_object.hpp
@@ -14,7 +14,7 @@ protected:
 
 public:
   SceneBaseObject() : m_surface(nullptr){};
-  SceneBaseObject(ObjectBaseSurface *surface, double spec_reflect = 1., double diff_reflect = 1., double amb_reflect = 1., double shine = 10.);
+  SceneBaseObject(ObjectBaseSurface *surface, double spec_reflect = 0.5, double diff_reflect = 0.5, double amb_reflect = 1., double shine = 100.);
   SceneBaseObject(const SceneBaseObject &obj) = default;
   SceneBaseObject(SceneBaseObject &&obj) = default;
   virtual ~SceneBaseObject(){};
