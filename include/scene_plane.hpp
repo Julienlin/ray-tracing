4f77f430f8ff47616f9b4c0778f7f332b387f911
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-09 11:40:11 +0100

    modifying constructor

--- include/scene_plane.hpp
+++ include/scene_plane.hpp
@@ -14,8 +14,8 @@ private:
     vector_t m_normal;
 
 public:
-    ScenePlane(ObjectBaseSurface *surface, double spec_reflect, double diff_reflect, double amb_reflect, double shine, position_t origin, vector_t e1, vector_t e2);
-    ScenePlane(ObjectBaseSurface *surface, position_t origin, vector_t e1, vector_t e2);
+    ScenePlane(ObjectBaseSurface *surface, position_t origin, vector_t e1, vector_t e2, double reflect = 0., double spec_reflect = 0.5, double diff_reflect = 0.5, double amb_reflect = 1, double shine = 10.);
+    // ScenePlane(ObjectBaseSurface *surface, position_t origin, vector_t e1, vector_t e2);
     vector_t getNormal(const position_t &pos);
     double intersecDist(const Ray &ray);
     RGBColor getColor(position_t &pos) const;
