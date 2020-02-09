2549368cf33374622c7103fbcfebbead07634339
Julien Lin <julien.lin@ensta-paris.fr>
2020-02-09 17:37:28 +0100

    changing " to <

--- test/rayTracing.hpp
+++ test/rayTracing.hpp
@@ -1,15 +1,16 @@
 #ifndef __MAIN_H_
 #define __MAIN_H_
 
-#include "light_source.hpp"
-#include "objet_base_surface.hpp"
-#include "ray.hpp"
-#include "ray_engine.hpp"
-#include "scene_base_object.hpp"
-#include "scene_cube.hpp"
-#include "scene_sphere.hpp"
-#include "screen.hpp"
-#include "types.hpp"
+#include <light_source.hpp>
+#include <objet_base_surface.hpp>
+#include <ray.hpp>
+#include <ray_engine.hpp>
+#include <scene_base_object.hpp>
+#include <scene_cube.hpp>
+#include <scene_sphere.hpp>
+#include <screen.hpp>
+#include <types.hpp>
+#include <scene_triangle.hpp>
 #include <iostream>
 #include <scene_plane.hpp>
 #include <spdlog/sinks/basic_file_sink.h>
